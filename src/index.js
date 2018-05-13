8b4bd7964f6a8b15a66aa99e02345af0cb6944fb
garyvry@outlook.com <garyvry@outlook.com>
2018-05-13 12:12:02 +0100

    Tidy up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import otherSquare, { add, name } from './utilities'
-import myScream from './scream'
+import otherSquare, { add } from './utilities'
 
-console.log('index.js')
 console.log(add(32, 1))
-console.log(name)
-console.log(myScream('where the hell is everyone'))
 console.log(otherSquare(9))
--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,5 @@
-console.log('utilities.js')
-
 const add = (a, b) => a + b
 
-const name = 'Gary'
-
 const square = x => x * x
-console.log('from my code')
-export { add, name, square as default }
+
+export { add, square as default }
