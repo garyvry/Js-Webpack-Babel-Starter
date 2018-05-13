eb3f057b463acc1671a7ba05995537d86c119a92
garyvry@outlook.com <garyvry@outlook.com>
2018-05-13 11:04:57 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import otherSquare, { add, name } from './utilities'
+import myScream from './scream'
+
+console.log('index.js')
+console.log(add(32, 1))
+console.log(name)
+console.log(myScream('where the hell is everyone'))
+console.log(otherSquare(9))
